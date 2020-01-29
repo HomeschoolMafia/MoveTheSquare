c4308f6f8d2cf6e73ee6de9e012410bbf932bbe9
Gabriel <gholley@ycp.edu>
2020-01-29 12:12:15 -0500

    Changed size of square, color of square, and movement speed

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -30,7 +30,8 @@ public class GameView extends JPanel {
 
 		// djh2-KEC119-21: changed from 30 to 45
 		// djh2-YCPlaptop: change from 45 to 100
-		this.timer = new Timer(1000 / 100, new ActionListener() {
+		// gholley: change from 100 to 300
+		this.timer = new Timer(1000 / 300, new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -66,7 +67,8 @@ public class GameView extends JPanel {
 		
 		// djh2-KEC110-21: changed from GREEN to RED
 		// djh2-YCPlaptop: change from RED to YELLOW
-		g.setColor(Color.YELLOW);
+		// gholley: change from YELLOW to BLACK
+		g.setColor(Color.BLACK);
 
 		Square square = model.getSquare();
 		
@@ -81,11 +83,12 @@ public class GameView extends JPanel {
 				model.setWidth(640.0);
 				model.setHeight(480.0);
 				
+				// gholley: Changed square from 40 to 100(height and width)
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
+				square.setWidth(100.0);
+				square.setHeight(100.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
